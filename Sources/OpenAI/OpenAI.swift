4f44c6d4cfb173518fd78988bfb9d184b6bb69c0
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-02-20 13:31:22 -0800

    :alien: (Compatability) Make model optional to allow using TogetherAI endpoint
    TogetherAI's API mocks OpenAI's, however the model is not returned in chat stream responses, thus it must be made optional in order to proceed without erroring.
    
    :alien: (Groq) Allow paths in model host to enable Groq API support
    
    Assumes that there is no trailing slash
    
    :sparkles: (Invisibility) Add support for invisibility metadata
    
    for i.inc
    
    :bug: (Streaming) Fix streamed JSON chunks where content has "data:" caused a parsing error
    
    There are still failure cases where its "
    data:" but is okay for now

--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -7,23 +7,21 @@
 
 import Foundation
 #if canImport(FoundationNetworking)
-import FoundationNetworking
+    import FoundationNetworking
 #endif
 
-final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
-
+public final class OpenAI: OpenAIProtocol, @unchecked Sendable {
     public struct Configuration: Sendable {
-        
         /// OpenAI API token. See https://platform.openai.com/docs/api-reference/authentication
         ///
         /// Set the token when the app works directly with OpenAI API. This approach is generally unsafe and should not be used in production.
         /// Default value is nil. It would work when you have a proxy server that manages authentication.
         /// See https://github.com/MacPaw/OpenAI/discussions/116 for more info
         public let token: String?
-        
+
         /// Optional OpenAI organization identifier. See https://platform.openai.com/docs/api-reference/authentication
         public let organizationIdentifier: String?
-        
+
         /// API host. Set this property if you use some kind of proxy or your own server. Default is api.openai.com
         public let host: String
 
@@ -32,10 +30,10 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
 
         public let port: Int
         public let scheme: String
-        
+
         /// Default request timeout
         public let timeoutInterval: TimeInterval
-        
+
         /// Headers to set on a request.
         ///
         /// Value from this dict would set on any request sent by SDK.
@@ -44,10 +42,10 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
         ///
         /// Currently SDK sets such fields: Authorization, Content-Type, OpenAI-Organization.
         public let customHeaders: [String: String]
-        
+
         public let parsingOptions: ParsingOptions
-        
-        public init(token: String?, organizationIdentifier: String? = nil, host: String = "api.openai.com", port: Int = 443, scheme: String = "https", basePath: String = "/v1", timeoutInterval: TimeInterval = 60.0, customHeaders: [String: String] = [:], parsingOptions: ParsingOptions = []) {
+
+        public init(token: String?, organizationIdentifier: String? = nil, host: String = "api.openai.com", port: Int = 443, scheme: String = "https", basePath: String = "", timeoutInterval: TimeInterval = 60.0, customHeaders: [String: String] = [:], parsingOptions: ParsingOptions = []) {
             self.token = token
             self.organizationIdentifier = organizationIdentifier
             self.host = host
@@ -59,12 +57,12 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             self.parsingOptions = parsingOptions
         }
     }
-    
+
     let client: Client
     let streamingClient: StreamingClient
     let asyncClient: AsyncClient
     let combineClient: CombineClient
-    
+
     public let configuration: Configuration
     public let responses: ResponsesEndpointProtocol
 
@@ -76,7 +74,7 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             sslStreamingDelegate: nil
         )
     }
-    
+
     public convenience init(configuration: Configuration) {
         self.init(
             configuration: configuration,
@@ -85,7 +83,7 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             sslStreamingDelegate: nil
         )
     }
-    
+
     public convenience init(
         configuration: Configuration,
         session: URLSession = URLSession.shared,
@@ -97,7 +95,7 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             parsingOptions: configuration.parsingOptions,
             sslDelegate: sslStreamingDelegate
         )
-        
+
         self.init(
             configuration: configuration,
             session: session,
@@ -115,44 +113,44 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
         middlewares: [OpenAIMiddleware] = []
     ) {
         self.configuration = configuration
-        
+
         let dataTaskFactory = DataTaskFactory(
             session: session,
             responseHandler: .init(middlewares: middlewares, configuration: configuration)
         )
-        
-        self.client = .init(
+
+        client = .init(
             configuration: configuration,
             session: session,
             middlewares: middlewares,
             dataTaskFactory: dataTaskFactory,
             cancellablesFactory: cancellablesFactory
         )
-        
-        self.streamingClient = .init(
+
+        streamingClient = .init(
             configuration: configuration,
             streamingSessionFactory: streamingSessionFactory,
             middlewares: middlewares,
             cancellablesFactory: cancellablesFactory,
             executionSerializer: executionSerializer
         )
-        
-        self.asyncClient = .init(
+
+        asyncClient = .init(
             configuration: configuration,
             session: session,
             middlewares: middlewares,
             dataTaskFactory: dataTaskFactory,
             responseHandler: .init(middlewares: middlewares, configuration: configuration)
         )
-        
-        self.combineClient = .init(
+
+        combineClient = .init(
             configuration: configuration,
             session: session,
             middlewares: middlewares,
             responseHandler: .init(middlewares: middlewares, configuration: configuration)
         )
-        
-        self.responses = ResponsesEndpoint(
+
+        responses = ResponsesEndpoint(
             client: client,
             streamingClient: streamingClient,
             asyncClient: asyncClient,
@@ -160,7 +158,7 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             configuration: configuration
         )
     }
-    
+
     public func threadsAddMessage(
         threadId: String,
         query: MessageQuery,
@@ -171,7 +169,7 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             completion: completion
         )
     }
-    
+
     public func threadsMessages(
         threadId: String,
         before: String? = nil,
@@ -182,14 +180,14 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             completion: completion
         )
     }
-    
+
     public func runRetrieve(threadId: String, runId: String, completion: @escaping @Sendable (Result<RunResult, Error>) -> Void) -> CancellableRequest {
         performRequest(
             request: makeRunRetrieveRequest(threadId, runId),
             completion: completion
         )
     }
-    
+
     public func runRetrieveSteps(
         threadId: String,
         runId: String,
@@ -201,7 +199,7 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             completion: completion
         )
     }
-    
+
     public func runSubmitToolOutputs(
         threadId: String,
         runId: String,
@@ -213,7 +211,7 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             completion: completion
         )
     }
-    
+
     public func runs(threadId: String, query: RunsQuery, completion: @escaping @Sendable (Result<RunResult, Error>) -> Void) -> CancellableRequest {
         performRequest(
             request: makeRunsRequest(threadId, query),
@@ -227,35 +225,35 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             completion: completion
         )
     }
-    
+
     public func threadRun(query: ThreadRunQuery, completion: @escaping @Sendable (Result<RunResult, Error>) -> Void) -> CancellableRequest {
         performRequest(
             request: makeThreadRunRequest(query),
             completion: completion
         )
     }
-    
+
     public func assistants(after: String? = nil, completion: @escaping @Sendable (Result<AssistantsResult, Error>) -> Void) -> CancellableRequest {
         performRequest(
             request: makeAssistantsRequest(after),
             completion: completion
         )
     }
-    
+
     public func assistantCreate(query: AssistantsQuery, completion: @escaping @Sendable (Result<AssistantResult, Error>) -> Void) -> CancellableRequest {
         performRequest(
             request: makeAssistantCreateRequest(query),
             completion: completion
         )
     }
-    
+
     public func assistantModify(query: AssistantsQuery, assistantId: String, completion: @escaping @Sendable (Result<AssistantResult, Error>) -> Void) -> CancellableRequest {
         performRequest(
             request: makeAssistantModifyRequest(assistantId, query),
             completion: completion
         )
     }
-    
+
     public func files(query: FilesQuery, completion: @escaping @Sendable (Result<FilesResult, Error>) -> Void) -> CancellableRequest {
         performRequest(
             request: makeFilesRequest(query: query),
@@ -266,23 +264,23 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
     public func images(query: ImagesQuery, completion: @escaping @Sendable (Result<ImagesResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeImagesRequest(query: query), completion: completion)
     }
-    
+
     public func imageEdits(query: ImageEditsQuery, completion: @escaping @Sendable (Result<ImagesResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeImageEditsRequest(query: query), completion: completion)
     }
-    
+
     public func imageVariations(query: ImageVariationsQuery, completion: @escaping @Sendable (Result<ImagesResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeImageVariationsRequest(query: query), completion: completion)
     }
-    
+
     public func embeddings(query: EmbeddingsQuery, completion: @escaping @Sendable (Result<EmbeddingsResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeEmbeddingsRequest(query: query), completion: completion)
     }
-    
+
     public func chats(query: ChatQuery, completion: @escaping @Sendable (Result<ChatResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeChatsRequest(query: query.makeNonStreamable()), completion: completion)
     }
-    
+
     public func chatsStream(query: ChatQuery, onResult: @escaping @Sendable (Result<ChatStreamResult, Error>) -> Void, completion: (@Sendable (Error?) -> Void)?) -> CancellableRequest {
         performStreamingRequest(
             request: JSONRequest<ChatStreamResult>(body: query.makeStreamable(), url: buildURL(path: .chats)),
@@ -290,23 +288,23 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             completion: completion
         )
     }
-    
+
     public func model(query: ModelQuery, completion: @escaping @Sendable (Result<ModelResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeModelRequest(query: query), completion: completion)
     }
-    
+
     public func models(completion: @escaping @Sendable (Result<ModelsResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeModelsRequest(), completion: completion)
     }
-    
+
     public func moderations(query: ModerationsQuery, completion: @escaping @Sendable (Result<ModerationsResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeModerationsRequest(query: query), completion: completion)
     }
-    
+
     public func audioTranscriptions(query: AudioTranscriptionQuery, completion: @escaping @Sendable (Result<AudioTranscriptionResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeAudioTranscriptionsRequest(query: query), completion: completion)
     }
-    
+
     public func audioTranscriptionsVerbose(
         query: AudioTranscriptionQuery,
         completion: @escaping @Sendable (Result<AudioTranscriptionVerboseResult, Error>) -> Void
@@ -315,10 +313,10 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             completion(.failure(AudioTranscriptionError.invalidQuery(expectedResponseFormat: .verboseJson)))
             return NoOpCancellableRequest()
         }
-        
+
         return performRequest(request: makeAudioTranscriptionsRequest(query: query), completion: completion)
     }
-    
+
     public func audioTranscriptionStream(query: AudioTranscriptionQuery, onResult: @escaping @Sendable (Result<AudioTranscriptionStreamResult, Error>) -> Void, completion: (@Sendable (Error?) -> Void)?) -> CancellableRequest {
         performStreamingRequest(
             request: makeAudioTranscriptionsRequest(query: query.makeStreamable()),
@@ -326,15 +324,15 @@ final public class OpenAI: OpenAIProtocol, @unchecked Sendable {
             completion: completion
         )
     }
-    
+
     public func audioTranslations(query: AudioTranslationQuery, completion: @escaping @Sendable (Result<AudioTranslationResult, Error>) -> Void) -> CancellableRequest {
         performRequest(request: makeAudioTranslationsRequest(query: query), completion: completion)
     }
-    
+
     public func audioCreateSpeech(query: AudioSpeechQuery, completion: @escaping @Sendable (Result<AudioSpeechResult, Error>) -> Void) -> CancellableRequest {
         performSpeechRequest(request: makeAudioCreateSpeechRequest(query: query), completion: completion)
     }
-    
+
     public func audioCreateSpeechStream(query: AudioSpeechQuery, onResult: @escaping @Sendable (Result<AudioSpeechResult, Error>) -> Void, completion: (@Sendable (Error?) -> Void)?) -> CancellableRequest {
         performSpeechStreamingRequest(
             request: JSONRequest<AudioSpeechResult>(body: query, url: buildURL(path: .audioSpeech)),
@@ -351,7 +349,7 @@ extension OpenAI {
     ) -> CancellableRequest {
         client.performRequest(request: request, completion: completion)
     }
-    
+
     func performStreamingRequest<ResultType: Codable & Sendable>(
         request: any URLRequestBuildable,
         onResult: @escaping @Sendable (Result<ResultType, Error>) -> Void,
@@ -359,11 +357,11 @@ extension OpenAI {
     ) -> CancellableRequest {
         streamingClient.performStreamingRequest(request: request, onResult: onResult, completion: completion)
     }
-    
+
     func performSpeechRequest(request: any URLRequestBuildable, completion: @escaping @Sendable (Result<AudioSpeechResult, Error>) -> Void) -> CancellableRequest {
         client.performSpeechRequest(request: request, completion: completion)
     }
-    
+
     func performSpeechStreamingRequest(
         request: any URLRequestBuildable,
         onResult: @escaping @Sendable (Result<AudioSpeechResult, Error>) -> Void,
@@ -379,7 +377,7 @@ extension OpenAI {
             .buildURL()
     }
 
-    func buildRunsURL(path: String, threadId: String, before: String? = nil) -> URL {
+    func buildRunsURL(path: String, threadId: String, before _: String? = nil) -> URL {
         RunsURLBuilder(configuration: configuration, path: .init(stringValue: path), threadId: threadId)
             .buildURL()
     }
@@ -408,27 +406,28 @@ extension APIPath {
         static func runSubmitToolOutputs(threadId: String, runId: String) -> Assistants {
             Assistants(stringValue: "/threads/\(threadId)/runs/\(runId)/submit_tool_outputs")
         }
+
         static let threadsMessages = Assistants(stringValue: "/threads/THREAD_ID/messages")
         static let files = Assistants(stringValue: "/files")
-        
+
         let stringValue: String
     }
-    
+
     struct Responses {
         static let createModelResponse = Responses(stringValue: "/responses")
-        
+
         static func getModelResponse(responseId: String) -> Responses {
             .init(stringValue: "/responses/\(responseId)")
         }
-        
+
         static func deleteModelResponse(responseId: String) -> Responses {
             .init(stringValue: "/responses/\(responseId)")
         }
-        
+
         static func listInputItems(responseId: String) -> Responses {
             .init(stringValue: "responses/\(responseId)/input_items")
         }
-        
+
         let stringValue: String
     }
 
@@ -436,15 +435,15 @@ extension APIPath {
     static let chats = "/chat/completions"
     static let models = "/models"
     static let moderations = "/moderations"
-    
+
     static let audioSpeech = "/audio/speech"
     static let audioTranscriptions = "/audio/transcriptions"
     static let audioTranslations = "/audio/translations"
-    
+
     static let images = "/images/generations"
     static let imageEdits = "/images/edits"
     static let imageVariations = "/images/variations"
-    
+
     func withPath(_ path: String) -> String {
         self + "/" + path
     }
