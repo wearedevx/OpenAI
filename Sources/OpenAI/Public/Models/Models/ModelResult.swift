830a9b934276494f86a3b4954df691b3afbec5c6
Gaël Philippe <gaelph@gmail.com>
2024-06-18 15:20:15 +0200

    fix(models): add support for openrouter

--- Sources/OpenAI/Public/Models/Models/ModelResult.swift
+++ Sources/OpenAI/Public/Models/Models/ModelResult.swift
@@ -1,5 +1,5 @@
 //
-//  Model.swift
+//  ModelResult.swift
 //
 //
 //  Created by Aled Samuel on 08/04/2023.
@@ -9,15 +9,14 @@ import Foundation
 
 /// The model object matching the specified ID.
 public struct ModelResult: Codable, Equatable {
-
     /// The model identifier, which can be referenced in the API endpoints.
     public let id: String
     /// The Unix timestamp (in seconds) when the model was created.
-    public let created: TimeInterval
+    public var created: TimeInterval?
     /// The object type, which is always "model".
-    public let object: String
+    public var object: String = "model"
     /// The organization that owns the model.
-    public let ownedBy: String
+    public var ownedBy: String = "-"
 
     public enum CodingKeys: String, CodingKey {
         case id
