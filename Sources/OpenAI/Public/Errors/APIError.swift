870183e9be033e3a909dc93f4aac5e55df461e94
Gaël Philippe <gaelph@gmail.com>
2024-09-26 16:52:06 +0200

    fix(api error): make the type optional because it may be missing

--- Sources/OpenAI/Public/Errors/APIError.swift
+++ Sources/OpenAI/Public/Errors/APIError.swift
@@ -14,46 +14,45 @@ public enum OpenAIError: Error {
 
 public struct APIError: Error, Decodable, Equatable {
     public let message: String
-    public let type: String
+    public let type: String?
     public let param: String?
     public let code: String?
 
-  public init(message: String, type: String, param: String?, code: String?) {
-    self.message = message
-    self.type = type
-    self.param = param
-    self.code = code
-  }
-
-  enum CodingKeys: CodingKey {
-    case message
-    case type
-    case param
-    case code
-  }
-
-  public init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-
-    //
-    // message can be String or [String].
-    //
-    if let string = try? container.decode(String.self, forKey: .message) {
-      self.message = string
-    } else if let array = try? container.decode([String].self, forKey: .message) {
-      self.message = array.joined(separator: "\n")
-    } else {
-      throw DecodingError.typeMismatch(String.self, .init(codingPath: [CodingKeys.message], debugDescription: "message: expected String or [String]"))
+    public init(message: String, type: String, param: String?, code: String?) {
+        self.message = message
+        self.type = type
+        self.param = param
+        self.code = code
     }
 
-    self.type = try container.decode(String.self, forKey: .type)
-    self.param = try container.decodeIfPresent(String.self, forKey: .param)
-    self.code = try container.decodeIfPresent(String.self, forKey: .code)
-  }
+    enum CodingKeys: CodingKey {
+        case message
+        case type
+        case param
+        case code
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        //
+        // message can be String or [String].
+        //
+        if let string = try? container.decode(String.self, forKey: .message) {
+            message = string
+        } else if let array = try? container.decode([String].self, forKey: .message) {
+            message = array.joined(separator: "\n")
+        } else {
+            throw DecodingError.typeMismatch(String.self, .init(codingPath: [CodingKeys.message], debugDescription: "message: expected String or [String]"))
+        }
+
+        type = try container.decodeIfPresent(String.self, forKey: .type)
+        param = try container.decodeIfPresent(String.self, forKey: .param)
+        code = try container.decodeIfPresent(String.self, forKey: .code)
+    }
 }
 
 extension APIError: LocalizedError {
-
     public var errorDescription: String? {
         return message
     }
@@ -64,7 +63,6 @@ public struct APIErrorResponse: Error, Decodable, Equatable {
 }
 
 extension APIErrorResponse: LocalizedError {
-
     public var errorDescription: String? {
         return error.errorDescription
     }
